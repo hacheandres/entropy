ce32f222b414b4fd60fbd38ead147e91814085bd
H. Andrés Tournour <andrestournour@gmail.com>
2021-04-24 15:18:04 -0300

    Test files and run.bat

---
+++ run.bat
@@ -0,0 +1,16 @@
+.\entropy.exe -b 1024 .\test.pdf
+
+.\entropy.exe -b 1024000 .\test2.pdf
+
+.\entropy.exe -b 1024 .\test.css
+
+.\entropy.exe -b 10240 .\test.txt
+
+.\entropy.exe -b 10240 .\test.exe
+
+.\entropy.exe -b 1024000 .\test.7z
+
+pause 
+
+
+
