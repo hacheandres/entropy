0608e7abc7e89e634fa15f6bb19a2f714f349102
H. Andrés Tournour <andrestournour@gmail.com>
2021-04-24 13:38:29 -0300

    v1.0 funcional

--- entropy.cpp
+++ entropy.cpp
@@ -1,9 +1,64 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <fstream>
+#include <iterator>
+#include <vector>
+#include <cmath>
+
 
 using namespace std ; 
 #define BLOCKSIZE_DEFAULT 1024
+
+bool arguments_process( int _argc, char *_argv[] , int & _blocksize , string & _filename );
+float entropy_block( const std::vector<char> & , int );
+void entropy_file( ifstream &, vector<float> &, int, int &, int &);
+
+int main( int argc, char* argv[])
+{
+	int blocksize = 0 ; 
+	string filename = ""; 
+
+	if( arguments_process( argc, argv, blocksize , filename ) == false )
+	{
+		cerr << "Error: Wrong arguments" << endl ;
+		return -1 ;
+	}
+
+	cout << filename << " " << blocksize << endl ;
+
+	ifstream input( filename , ios::binary);
+	if( input.is_open() == false )
+	{
+		cerr << "Cannot open the file: " << filename << endl ;
+		return -1 ;
+	}
+
+	std::vector<float> entropy_by_block ;
+	int lowentropy = 0 , highentropy = 0;
+
+	entropy_file( input, entropy_by_block, blocksize, lowentropy, highentropy );
+	input.close() ;
+
+	//Print report 
+
+	cout << "Entropy report for file: " << filename << endl ;
+	int nblocks = entropy_by_block.size(); 
+
+	cout.precision(2);
+	cout << "block# \t\t entropy" << endl ;
+	
+	for (int i = 0 ; i < nblocks ; i++ )
+	{
+		cout << i << "\t\t" << entropy_by_block[i] << endl ;
+	}
+
+	cout << "Low entropy blocks: " << lowentropy << endl ;
+	cout << "High entropy blocks: " << highentropy << endl ;
+ 
+	return 0 ;
+}
+
 
 bool arguments_process( int _argc, char *_argv[] , int & _blocksize , string & _filename )
 {
@@ -65,24 +120,69 @@ bool arguments_process( int _argc, char *_argv[] , int & _blocksize , string & _
 	return ret ;
 }
 
-int main( int argc, char* argv[])
-{
-	int blocksize = 0 ; 
-	string filename = ""; 
 
-	if( arguments_process( argc, argv, blocksize , filename ) == false )
+float entropy_block( const char * buf, int n )
+{ 
+	#define BYTES_MAX_VALUE 256 
+	
+	std::vector<int> bytes_count(BYTES_MAX_VALUE, 0) ;	//Llevare la cuenta de las apariciones de cada byte	
+	float ent = 0.0, prob = 0.0; 
+
+	unsigned char byte = 0 ;
+	for( int i = 0 ; i < n ; i++ )
 	{
-		cerr << "Error: Wrong arguments" << endl ;
-		return -1 ;
+		byte = (unsigned char ) buf[i] ;
+		/*Uso el valor del byte para indexar el vector bytes_count y así incrementar 
+		el valor de dicha posición, de esta forma llevo la cuenta de las apariciones
+		de cada byte*/
+		bytes_count[ byte ]++ ;
+	} 
+
+	for( int i = 0 ; i < BYTES_MAX_VALUE ; i++ )
+	{
+		if( bytes_count[i] != 0 )	//Si el byte no aparecio en la secuencia no aporta a la entropia
+		{
+			prob = (float) bytes_count[i] / n ;
+			ent -= prob * log2( prob ); 
+		}
 	}
 
-	cout << filename << " " << blocksize << endl ;
-	
-	cout << "Hi Disarmista" << endl ;
-	return 0 ;
+	cout << endl ;
+
+	return ent ;
+
 }
 
+void entropy_file( ifstream & _input, vector<float> & _ent_by_block , int _blocksize, int & _lowe, int & _highe)
+{
 
+	char * block = new char[_blocksize] ;	//Guardare los bytes leidos de a bloques
+	int nread = 0 ;				//La cantidad de bytes leidos, por si lei menos de un bloque entero
+	float ent = 0.0 ;
 
+	if( block == NULL )
+		return ;
 
+	_input.read ( block , _blocksize );
+	nread = _input.gcount() ;
+	
+	while( nread > 0 )
+	{
+		// cout << "nread: " << nread << endl ;
+		
+		ent = entropy_block( block , nread ) ;
+		_ent_by_block.push_back( ent );
+
+		if( ent > 7 )
+			_highe++;
+		else if ( ent < 2)
+			_lowe++ ;
+
+		_input.read ( & block[0], _blocksize );
+		nread = _input.gcount() ;
+	}
+
+	delete block ;
+	
+}
 
