85d6e0363955aa4553a07228b194d95630b55615
H. Andrés Tournour <andrestournour@gmail.com>
2021-04-24 22:22:48 -0300

    Run bat con algunos casos erroneos

--- run.bat
+++ run.bat
@@ -10,6 +10,13 @@
 
 .\entropy.exe -b 1024000 .\test.7z
 
+.\entropy.exe -b 10240 .\noexiste.txt
+
+.\entropy.exe -b 10240 .\noexiste.txt argdemas
+
+.\entropy.exe -c 10240 .\noexiste.txt
+
+
 pause 
 
 
